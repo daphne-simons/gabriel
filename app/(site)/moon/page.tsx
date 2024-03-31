20baaef87b7088a3d4c0ef98ad34d8046ab63cd8
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-03-31 16:51:55 +1300

    remove useEffect/useState, import getMoon query

--- app/(site)/moon/page.tsx
+++ app/(site)/moon/page.tsx
@@ -1,48 +1,26 @@
 'use client'
-import { useEffect, useState } from 'react'
-import { MoonApiResponse, MoonConfig } from '../models/models'
+
+import getMoon from '@/query/utils/getMoonData'
+import { useQuery } from '@tanstack/react-query'
+
 export default function Moon() {
-  // Add other properties as needed
-  const [moonData, setMoonData] = useState<MoonApiResponse | null>(null)
+  // useQuery to getMoon() data from query utils
+  const {
+    data: moonData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['moon'],
+    queryFn: () => getMoon(),
+  })
 
-  //  Fetch Moon data from API:
-  useEffect(() => {
-    const loadMoonPhases = async (obj: MoonConfig) => {
-      const gets = []
-      for (let i in obj) {
-        gets.push(i + '=' + encodeURIComponent(obj[i]))
-      }
-      const url = 'http://www.icalendar37.net/lunar/api/?' + gets.join('&')
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
-      try {
-        const response = await fetch(url)
-        if (!response.ok) {
-          throw new Error('Network response was not ok.')
-        }
-        const data = await response.json()
-        console.log(data)
-        setMoonData(data)
-        // todaysMoon(data)
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error)
-      }
-    }
-
-    const configMoon = {
-      lang: 'en', // 'ca' 'de' 'en' 'es' 'fr' 'it' 'pl' 'pt' 'ru' 'zh' (*)
-      month: new Date().getMonth() + 1, // 1 - 12
-      year: new Date().getFullYear(),
-      size: 100, // pixels - size of moon svg
-      lightColor: '#FFFF88', // CSS color
-      shadeColor: '#111111', // CSS color
-      sizeQuarter: 40, // pixels - size of moon svg
-      texturize: false, // true - false
-      // LDZ: Math.floor(new Date().getTime() / 1000), // rounds the percentage number of moon phase.
-    }
-
-    loadMoonPhases(configMoon)
-  }, []) // Empty dependency array to run once on component mount
-
+  if (isError) {
+    return <div>Sorry There was an Error</div>
+  }
   return (
     <div>
       <h1>Moon Widget</h1>
