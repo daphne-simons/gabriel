8c049189027532fa9c6af98a5dcb592c004d7b30
Daphne Simons <daphnejasminesimons@gmail.com>
2024-04-07 10:26:20 +1200

    ugly version of css and moondata working

--- app/(site)/moon/page.tsx
+++ app/(site)/moon/page.tsx
@@ -25,26 +25,44 @@ export default function Moon() {
   if (moonData) {
     const rawLighting = moonData.phase[new Date().getDate()].lighting
     const lightValue = Math.round(rawLighting)
-    // some kind of code probably an object stored as a state value, that runs on useEffect pageload, where it listens to the moonApi data, and returns a certain tailwind class based on the lighting value. I think it goes from 0 - 100. So if I write some kind of loop that will create a series a css values, depending on the number. Maybe it could actually be rounded a bit. Ie, if the number is from 0-5: have this class, and 5-10: another class, etc.
-    const lightingClasses = {
-      'bg-yellow-400': lightValue >= 0 && lightValue < 5,
-      'bg-yellow-500': lightValue >= 5 && lightValue < 10,
-      'bg-yellow-600': lightValue >= 10 && lightValue < 15,
-      'bg-yellow-700': lightValue >= 15 && lightValue < 20,
-      'bg-yellow-800': lightValue >= 20 && lightValue < 25,
-      'bg-yellow-900': lightValue >= 25 && lightValue < 30,
-      'bg-orange-400': lightValue >= 30 && lightValue < 35,
+    // Logic to set a tailwind bg-color value in relation to moon phase
+    let bgValue = ''
+    if (lightValue < 10) {
+      bgValue = 'bg-gray-950'
     }
+    if (lightValue >= 10 && lightValue < 20) {
+      bgValue = 'bg-gray-900'
+    }
+    if (lightValue >= 20 && lightValue < 30) {
+      bgValue = 'bg-gray-800'
+    }
+    if (lightValue >= 30 && lightValue < 40) {
+      bgValue = 'bg-gray-700'
+    }
+    if (lightValue >= 40 && lightValue < 50) {
+      bgValue = 'bg-gray-600'
+    }
+    if (lightValue >= 50 && lightValue < 60) {
+      bgValue = 'bg-gray-500'
+    }
+    if (lightValue >= 60 && lightValue < 70) {
+      bgValue = 'bg-gray-400'
+    }
+    if (lightValue >= 70 && lightValue < 80) {
+      bgValue = 'bg-gray-300'
+    }
+    if (lightValue >= 80 && lightValue < 90) {
+      bgValue = 'bg-gray-200'
+    }
+    if (lightValue >= 90 && lightValue < 100) {
+      bgValue = 'bg-gray-100'
+    }
+    console.log(bgValue)
 
     return (
-      <div>
-        <div
-        // className={`h-16 w-16 rounded-full ${
-        //   lightingClasses[
-        //     Object.keys(lightingClasses).find((key) => lightingClasses[key])
-        //   ]
-        // }`}
-        ></div>
+      <div
+        className={`flex flex-col items-center justify-center text-center ${bgValue}`}
+      >
         <h1>Moon Widget</h1>
         <div className="moon">
           <div>{moonData.nameDay[new Date().getDay()]}</div>
