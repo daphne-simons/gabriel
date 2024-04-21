47bffc3eb608dfc0809c782a4fe3721154ccc0d2
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-04-22 10:20:46 +1200

    fix contact page send email

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,24 +1,25 @@
-// import EmailTemplate from '../components/Email'
-// import { Resend } from 'resend'
+import EmailTemplate from '../components/Email'
+import { Resend } from 'resend'
 
-// const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
-// export default async function Contact() {
-//   async function send() {
-//     // Server actions to do the server side email sending.
-//     'use server'
-//     await resend.emails.send({
-//       // From needs to be a verified domain email
-//       from: 'Acme <onboarding@resend.dev>',
-//       // This can be whatever email you want to send to
-//       to: 'daphnejasminesimons@gmail.com',
-//       subject: 'Hello Gabriel client',
-//       react: EmailTemplate({ firstName: 'Jane2' }) as React.ReactElement,
-//     })
-//   }
-//   return (
-//     <form action={send}>
-//       <button type="submit">Send Email</button>
-//     </form>
-//   )
-// }
+export default async function Contact() {
+  async function send(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault() // Prevent the default form submission
+    // Server actions to do the server side email sending.
+    ;('use server')
+    await resend.emails.send({
+      // From needs to be a verified domain email
+      from: 'Acme <onboarding@resend.dev>',
+      // This can be whatever email you want to send to
+      to: 'daphnejasminesimons@gmail.com',
+      subject: 'Hello Gabriel client',
+      react: EmailTemplate({ firstName: 'Jane2' }),
+    })
+  }
+  return (
+    <form onSubmit={send}>
+      <button type="submit">Send Email</button>
+    </form>
+  )
+}
