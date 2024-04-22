773cdb2c381e00eb03d8ae77a4ef641146c48f5b
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-04-22 15:30:53 +1200

    font back to normal - semibold not an option

--- app/(site)/components/Header.tsx
+++ app/(site)/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header(props: Props) {
   console.log(color, size)
 
   const spanStyle = `absolute mx-auto py-4 flex border blur-lg ${color} bg-clip-text ${size} box-content text-transparent text-center select-none`
-  const h1Style = `relative top-0 py-4 justify-center flex items-center ${color} bg-clip-text ${size} text-transparent text-center select-auto font-medium`
+  const h1Style = `relative top-0 py-4 justify-center flex items-center ${color} bg-clip-text ${size} text-transparent text-center select-auto `
   return (
     <h1 className={serif.className}>
       <div className="">
