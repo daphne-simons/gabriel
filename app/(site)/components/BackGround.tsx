1999a046d7e9834418d614b38ba3dbd910883bf7
Daphne Simons <daphnejasminesimons@gmail.com>
2024-05-20 19:33:54 +1200

    texture of background more like wireframe

--- app/(site)/components/BackGround.tsx
+++ app/(site)/components/BackGround.tsx
@@ -34,16 +34,56 @@ export default function BackGround({
   }
   function calculateBgColor(lightVal: number) {
     const moonThemes = [
-      { bgValue: 'bg-gray-950', textValue: 'text-white' },
-      { bgValue: 'bg-gray-900', textValue: 'text-white' },
-      { bgValue: 'bg-gray-800', textValue: 'text-white' },
-      { bgValue: 'bg-gray-700', textValue: 'text-white' },
-      { bgValue: 'bg-gray-600', textValue: 'text-white' },
-      { bgValue: 'bg-gray-500', textValue: 'text-gray-950' },
-      { bgValue: 'bg-gray-400', textValue: 'text-gray-950' },
-      { bgValue: 'bg-gray-300', textValue: 'text-gray-800' },
-      { bgValue: 'bg-gray-200', textValue: 'text-gray-800' },
-      { bgValue: 'bg-gray-100', textValue: 'text-gray-700' },
+      {
+        bgValue: 'bg-gray-950',
+        textValue: 'text-white',
+        bgImg: `bg-[url('/moon-bg.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-900',
+        textValue: 'text-white',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-800',
+        textValue: 'text-white',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-700',
+        textValue: 'text-white',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-600',
+        textValue: 'text-white',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-500',
+        textValue: 'text-gray-950',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-400',
+        textValue: 'text-gray-950',
+        bgImg: `bg-[url('/moon-bg-80.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-300',
+        textValue: 'text-black',
+        bgImg: `bg-[url('/moon-bg-60.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-200',
+        textValue: 'text-black',
+        bgImg: `bg-[url('/moon-bg-60.png')]`,
+      },
+      {
+        bgValue: 'bg-gray-100',
+        textValue: 'text-black',
+        bgImg: `bg-[url('/moon-bg-60.png')]`,
+      },
     ]
 
     // Calculate index based on lightValue
@@ -59,7 +99,9 @@ export default function BackGround({
     const theme = calculateBgColor(lightValue)
 
     return (
-      <div className={`${theme.bgValue} ${theme.textValue}`}>{children}</div>
+      <div className={`${theme.bgValue} ${theme.textValue}`}>
+        <div className={`bg-cover ${theme.bgImg}`}>{children}</div>
+      </div>
     )
   }
 }
