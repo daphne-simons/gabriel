4b4f582a8e0cadf9b1ec3c87b8b803afe6dec6b0
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-06-01 16:01:19 +1200

    feeling lucky button bg dynamic

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
               <div>
                 {/* Feeling Lucky/ Contact Button */}
                 <Link href="/contact">
-                  <button className="px-4 py-2 bg-gray-300 rounded border border-[#202124] hover:border hover:border-gray-500 text-sm">
+                  <button className="px-4 py-2  rounded border border-gray-400 hover:border hover:border-gray-500 border-1 text-sm">
                     I&apos;m Feeling Lucky
                   </button>
                 </Link>
