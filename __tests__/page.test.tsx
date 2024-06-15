6dd497d30d998c081232cd2926dbe91c41838aec
Daphne Simons <daphnejasminesimons@gmail.com>
2024-06-15 16:32:18 +1200

    some basic test config

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Home from '../app/(site)/page'
+
+test('Page', () => {
+  render(<Home />)
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Gabriel' })
+  ).toBeDefined()
+})
