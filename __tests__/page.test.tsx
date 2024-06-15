bb9eba37e6b3a937656a4769923976a65781275a
Daphne Simons <daphnejasminesimons@gmail.com>
2024-06-15 16:38:10 +1200

    test works - only with HomeLogo

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,9 +1,11 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import Home from '../app/(site)/page'
-
+// import Home from '../app/(site)/page'
+import HomeLogo from '../app/(site)/components/HomeLogo'
 test('Page', () => {
-  render(<Home />)
+  // This one doesn't work for some reason
+  // render(<Home />)
+  render(<HomeLogo fontSettings={{ wght: 200, opsz: 72 }} />)
   expect(
     screen.getByRole('heading', { level: 1, name: 'Gabriel' })
   ).toBeDefined()
