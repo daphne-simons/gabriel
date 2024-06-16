f99dab4e55affe186f2466b2b34644cf9f6f5675
Daphne Simons <daphnejasminesimons@gmail.com>
2024-06-16 13:52:09 +1200

    make moon widget more realistic with "dial-a-moon" img from nasa

--- app/(site)/components/MoonWidget.tsx
+++ app/(site)/components/MoonWidget.tsx
@@ -4,6 +4,7 @@ import getMoon from '@/query/utils/getMoonData'
 import { useQuery } from '@tanstack/react-query'
 import MoonLoader from './MoonLoader'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function MoonWidget({ size }: { size: string }) {
   const {
@@ -23,44 +24,107 @@ export default function MoonWidget({ size }: { size: string }) {
   }
   if (moonData) {
     //  Gets the dynamic svg data from the moonData api query:
-    const moonPhaseSVG = moonData.phase[new Date().getDate()].svg // Assuming moonData is available in scope
-    console.log(moonData)
-    console.log(moonPhaseSVG)
-    // TODO: Make this moon svg NOT have an a tag in there.
-    // const newMoon = (
-    //   <svg width="100" height="100" viewBox="0 0 100 100">
-    //     <g>
-    //       <circle cx="50" cy="50" r="49" stroke="none" fill="#1d1d1d" />
-    //       <path
-    //         d="M 50 1 A 49,49 0 0,1 49,99 A -25.48,49 0 0,1 50,1"
-    //         stroke-width="0"
-    //         stroke="none"
-    //         fill="#f7f7c8"
-    //       />
-    //       <circle
-    //         cx="50"
-    //         cy="50"
-    //         r="49"
-    //         style="pointer-events:all;cursor:pointer"
-    //         stroke-width="0"
-    //         fill="transparent"
-    //       />
-    //     </g>
-    //   </svg>
-    // )
-    return (
-      <Link href="/moon" className="flex gap-8 hover:underline">
-        {moonData.phase[new Date().getDate()].phaseName}
-        <div className="-mt-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-            className="w-8 h-auto"
-          >
-            <g dangerouslySetInnerHTML={{ __html: moonPhaseSVG }} />{' '}
-          </svg>
-        </div>
-      </Link>
+    const moonPhaseSVG = moonData.phase[new Date().getDate()].svg
+    console.log(moonData.phase[new Date().getDate()])
+
+    // List out all moonPhases from api:
+    for (let key in moonData.phase) {
+      if (moonData.phase.hasOwnProperty(key)) {
+        // console.log(moonData.phase[key].phaseName)
+      }
+    }
+    const moonPhases = moonData.phase
+    // remove duplicate names
+    const phaseNamesSet = new Set()
+    for (let key in moonPhases) {
+      if (moonPhases.hasOwnProperty(key)) {
+        phaseNamesSet.add(moonPhases[key].phaseName)
+      }
+    }
+    // log them out
+    const uniquePhaseNames = Array.from(phaseNamesSet)
+    // console.log(uniquePhaseNames)
+
+    const imgArr = [
+      '/moon-imgs/new.png',
+      '/moon-imgs/waxing.png',
+      '/moon-imgs/first-q.png',
+      '/moon-imgs/waxing-g.png',
+      '/moon-imgs/full.png',
+      '/moon-imgs/waning-g.png',
+      '/moon-imgs/last-q.png',
+      '/moon-imgs/waning.png',
+    ]
+
+    const getMoonImgByLighting = (lighting: number) => {
+      if (lighting >= 0 && lighting <= 5) {
+        return {
+          img: imgArr[0],
+          phase: 'New Moon',
+        }
+      } else if (lighting >= 6 && lighting <= 42) {
+        return {
+          img: imgArr[1],
+          phase: 'Waxing',
+        }
+      } else if (lighting >= 43 && lighting <= 60) {
+        return {
+          img: imgArr[2],
+          phase: 'First Quarter',
+        }
+      } else if (lighting >= 61 && lighting <= 95) {
+        return {
+          img: imgArr[3],
+          phase: 'Waxing Gibbous',
+        }
+      } else if (lighting >= 96 && lighting <= 105) {
+        return {
+          img: imgArr[4],
+          phase: 'Full Moon',
+        }
+      } else if (lighting <= 97 && lighting >= 66) {
+        return {
+          img: imgArr[5],
+          phase: 'Waning Gibbous',
+        }
+      } else if (lighting <= 65 && lighting >= 44) {
+        return {
+          img: imgArr[6],
+          phase: 'Last Quarter',
+        }
+      } else if (lighting <= 43 && lighting >= 5) {
+        return {
+          img: imgArr[7],
+          phase: 'Waning',
+        }
+      }
+    }
+
+    const moonLighting = Number(
+      moonData.phase[new Date().getDate()].lighting.toFixed(0)
     )
+    console.log(moonLighting)
+
+    const moonImgData = getMoonImgByLighting(moonLighting)
+
+    if (moonImgData) {
+      return (
+        <div className="">
+          <Link
+            href="/moon"
+            className="gap-6 hover:underline flex items-center"
+          >
+            <h2>{moonImgData.phase}</h2>
+            <Image
+              src={moonImgData.img}
+              alt={moonImgData.phase}
+              width={30}
+              height={30}
+              className=""
+            />
+          </Link>
+        </div>
+      )
+    }
   }
 }
