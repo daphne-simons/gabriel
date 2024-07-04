58409c08c455b6b375ff2189311669f92f6b1a2f
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-07-04 13:13:56 +1200

    Storybooks looking good! All phases include bg and homelogo

--- stories/BackGround.stories.tsx
+++ stories/BackGround.stories.tsx
@@ -2,217 +2,104 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import {
   FullMoonBg,
+  FirstQuarterBg,
   LastQuarterBg,
   NewMoonBg,
   WaningBg,
   WaningGibbousBg,
   WaxingBg,
   WaxingGibbousBg,
-} from './BackGroundClones/MoonBgs'
+} from './FakeComponents/MoonBgs'
 import Image from 'next/image'
+import HomeLogo from '../app/(site)/components/Logos/HomeLogo'
 
 const meta: Meta<typeof NewMoonBg> = {
-  title: 'BackGrounds',
+  title: 'Phases HomePage',
   component: NewMoonBg,
 }
 
 type Story = StoryObj<typeof NewMoonBg>
 
-// const themes = [
-//   {
-//     phase: 'New Moon',
-//     bgValue: 'bg-gray-800',
-//     textValue: 'text-white',
-//     bgImg: 'bg-moon-bg',
-//   },
-//   {
-//     phase: 'Waxing',
-//     bgValue: 'bg-gray-500',
-//     textValue: 'text-white',
-//     bgImg: 'bg-moon-bg-80',
-//   },
-//   {
-//     phase: 'First Quarter',
-//     bgValue: 'bg-gray-300',
-//     textValue: 'text-white',
-//     bgImg: 'bg-moon-bg-80',
-//   },
-//   {
-//     phase: 'Waxing Gibbous',
-//     bgValue: 'bg-gray-200',
-//     textValue: 'text-white',
-//     bgImg: 'bg-moon-bg-60',
-//   },
-//   {
-//     phase: 'Full Moon',
-//     bgValue: 'bg-gray-100',
-//     textValue: 'text-white',
-//     bgImg: `{bg-[url('/moon-bg-60.png')]`,
-//   },
-//   {
-//     phase: 'Waning Gibbous',
-//     bgValue: 'bg-gray-200',
-//     textValue: 'text-gray-950',
-//     bgImg: 'bg-moon-bg-60',
-//   },
-//   {
-//     phase: 'Last Quarter',
-//     bgValue: 'bg-gray-300',
-//     textValue: 'text-gray-950',
-//     bgImg: 'bg-moon-bg-80',
-//   },
-//   {
-//     phase: 'Waning',
-//     bgValue: 'bg-gray-500',
-//     textValue: 'text-black',
-//     bgImg: 'bg-moon-bg-60',
-//   },
-// ]
-
 export const BG1: Story = {
   name: 'New Moon',
   render: () => (
-    <div className="">
-      <NewMoonBg />
-      <Image
-        src="/moon-bg.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <NewMoonBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </NewMoonBg>
   ),
 }
 
 export const BG2: Story = {
   name: 'Waxing',
   render: () => (
-    <div className="h-screen w-full">
-      <WaxingBg />
-      <Image
-        src="/moon-bg-80.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <WaxingBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </WaxingBg>
   ),
 }
 
 export const BG3: Story = {
   name: 'First Quarter',
   render: () => (
-    <div className="h-screen w-full">
-      <WaxingBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <FirstQuarterBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </FirstQuarterBg>
   ),
 }
 export const BG4: Story = {
   name: 'Waxing Gibbous',
   render: () => (
-    <div className="h-screen w-full">
-      <WaxingGibbousBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <WaxingGibbousBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </WaxingGibbousBg>
   ),
 }
 export const BG5: Story = {
   name: 'Full Moon',
   render: () => (
-    <div className="h-screen w-full ">
-      <FullMoonBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <FullMoonBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </FullMoonBg>
   ),
 }
 export const BG6: Story = {
   name: 'Waning Gibbous',
   render: () => (
-    <div className="h-screen w-full">
-      <WaningGibbousBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <WaningGibbousBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </WaningGibbousBg>
   ),
 }
 export const BG7: Story = {
   name: 'Last Quarter',
   render: () => (
-    <div className="h-screen w-full">
-      <LastQuarterBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <LastQuarterBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </LastQuarterBg>
   ),
 }
 export const BG8: Story = {
   name: 'Waning',
   render: () => (
-    <div className="h-screen w-full">
-      <WaningBg />
-      <Image
-        src="/moon-bg-60.png"
-        alt="moon"
-        layout="fill"
-        style={{
-          position: 'absolute',
-          objectFit: 'cover',
-          zIndex: 1,
-        }}
-      />
-    </div>
+    <WaningBg>
+      <div className="flex items-center justify-center h-screen">
+        <HomeLogo fontSettings={{ wght: 500, opsz: 50 }} />
+      </div>
+    </WaningBg>
   ),
 }
 export default meta
--- stories/FakeComponents/MoonBgs.tsx
+++ stories/FakeComponents/MoonBgs.tsx
@@ -1,93 +1,94 @@
-export function NewMoonBg() {
+import Image from 'next/image'
+export function NewMoonBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-newMoon fill-current text-skin-newMoon outline outline-outline-skin-newMoon outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg`}></div>
+        <div className={`bg-cover bg-moon-bg`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function WaxingBg() {
+export function WaxingBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-waxing fill-current text-skin-waxing outline outline-outline-skin-waxing outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-60`}></div>
+        <div className={`bg-cover bg-moon-bg-80`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function FirstQuarterBg() {
+export function FirstQuarterBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-firstQuarter fill-current text-skin-firstQuarter outline outline-outline-skin-firstQuarter outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-80`}></div>
+        <div className={`bg-cover bg-moon-bg-60`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function WaxingGibbousBg() {
+export function WaxingGibbousBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-waxingGibbous fill-current text-skin-waxingGibbous outline outline-outline-skin-waxingGibbous outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-60`}></div>
+        <div className={`bg-cover bg-moon-bg-60`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function FullMoonBg() {
+export function FullMoonBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-fullMoon fill-current text-skin-fullMoon outline outline-outline-skin-fullMoon outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-60`}></div>
+        <div className={`bg-cover bg-moon-bg-60`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function WaningGibbousBg() {
+export function WaningGibbousBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-waningGibbous fill-current text-skin-waningGibbous outline outline-outline-skin-waningGibbous outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-60`}></div>
+        <div className={`bg-cover bg-moon-bg-60`}>{children}</div>
       </div>
     </>
   )
 }
-export function LastQuarterBg() {
+export function LastQuarterBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-lastQuarter fill-current text-skin-lastQuarter outline outline-outline-skin-lastQuarter outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-80`}></div>
+        <div className={`bg-cover bg-moon-bg-60`}>{children}</div>
       </div>
     </>
   )
 }
 
-export function WaningBg() {
+export function WaningBg({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div
         className={`h-screen w-full bg-skin-waning fill-current text-skin-waning outline outline-outline-skin-waning outline-1`}
       >
-        <div className={`bg-cover bg-moon-bg-60`}></div>
+        <div className={`bg-cover bg-moon-bg-80`}>{children}</div>
       </div>
     </>
   )
