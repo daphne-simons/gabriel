37bab7af8df923cb8401ae49f67cad99d0abbcbd
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-10-11 10:57:59 +1300

    got outline working with extra gaurd function to check for string?.. but want to refactor

--- app/(site)/components/HomeSearchBar.tsx
+++ app/(site)/components/HomeSearchBar.tsx
@@ -27,100 +27,128 @@ export default function HomeSearchBar(theme: Props) {
   // BUG: this prints the right class, but it doesn't work when dynamically included inline
   console.log(theme.outlineColor) //
 
-  return (
-    <>
-      <div
-        // BUG: here is where it's not working when dynamic
-        className={
-          isOpen
-            ? `relative flex flex-col lg:w-1/2 md:w-2/3 sm:w-2/3 justify-start rounded-full pl-14 pr-14 py-2.5 outline-none shadow-none`
-            : `relative flex flex-col lg:w-1/2 md:w-2/3 sm:w-2/3 justify-start rounded-full pl-14 pr-14 py-2.5 outline outline-1
-            ${theme.outlineColor} hover:shadow-xl`
-        }
-        onClick={toggleDropdown}
-      >
-        {/* Main Search Bar */}
-        <div className="flex flex-row relative cursor-pointer">
-          <div className="">
-            {/* magnifying glass icon svg */}
-            <span
-              className={
-                !isOpen
-                  ? 'block h-6 w-6 -ml-[40px] mb-[5px] absolute'
-                  : 'h-6 w-6 -ml-[40px] mb-[5px] absolute opacity-0'
-              }
-            >
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 25 25"
-              >
-                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-              </svg>
-            </span>
-          </div>
-          {/* Search input */}
-          <p className={!isOpen ? 'block pl-1' : 'opacity-0 pl-1'}>
-            What are you looking for?
-          </p>
-        </div>
-        {/* Dropdown Menu */}
+  function checkOutlineTheme() {
+    let outlineClass = ''
+    if (theme.outlineColor === 'outline-skin-newMoon') {
+      outlineClass = `outline-skin-newMoon`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-waxing') {
+      outlineClass = `outline-skin-waxing`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-firstQuarter') {
+      outlineClass = `outline-skin-firstQuarter`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-waxingGibbous') {
+      outlineClass = `outline-skin-waxingGibbous`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-fullMoon') {
+      outlineClass = `outline-skin-fullMoon`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-waningGibbous') {
+      outlineClass = `outline-skin-waningGibbous`
+      return outlineClass as string
+    } else if (theme.outlineColor === 'outline-skin-lastQuarter') {
+      outlineClass = `outline-skin-lastQuarter`
+      return outlineClass as string
+    }
+  }
+
+  const outline = checkOutlineTheme()
+
+  if (outline)
+    return (
+      <>
         <div
-          id="dropdown"
-          className={`absolute z-10 pl-10 rounded-[22px] p-custom left-1/2 -translate-x-1/2 -translate-y-2.5 flex flex-col w-full ${
+          // BUG: here is where it's not working when dynamic
+          className={`relative flex flex-col lg:w-1/2 md:w-2/3 sm:w-2/3 justify-start rounded-full pl-14 pr-14 py-2.5 ${
             isOpen
-              ? 'block bg-transparent shadow-xl outline outline-1'
-              : 'hidden'
+              ? `outline-none shadow-none`
+              : `outline outline-1 hover:shadow-xl ${outline}`
           }`}
+          onClick={toggleDropdown}
         >
-          {/* Landing Option*/}
-          <div className="flex flex-row relative rounded-full gap-1.5 lg:w-1/2 md:w-2/3 sm:w-2/3 ">
-            <span className="h-6 w-6 -ml-[24px] mt-[8px] absolute">
-              <svg
-                focusable="false"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 25 25"
+          {/* Main Search Bar */}
+          <div className="flex flex-row relative cursor-pointer">
+            <div className="">
+              {/* magnifying glass icon svg */}
+              <span
+                className={
+                  !isOpen
+                    ? 'block h-6 w-6 -ml-[40px] mb-[5px] absolute'
+                    : 'h-6 w-6 -ml-[40px] mb-[5px] absolute opacity-0'
+                }
               >
-                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-              </svg>
-            </span>
-            <li className=" block px-3 pt-2 pb-4 pl-5">
-              What are you looking for?
-            </li>
-          </div>
-          <div className="translate-x-4 -translate-y-2.5 border-b w-[90%] h-1 border-gray-300"></div>
-          {/* Options - Map through services state */}
-          {services.map((service, index) => (
-            <div key={index} className=" ">
-              <div className="flex flex-row relative ">
-                <span className="h-6 w-6 -ml-[24px] mt-[8px] absolute">
-                  <svg
-                    focusable="false"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 25 25"
-                  >
-                    <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                  </svg>
-                </span>
-                <Link href={`/search-results?service=${service}`}>
-                  <li className="block px-5 py-2">
-                    I want{' '}
-                    <span className="font-roboto font-bold">{service}</span>
-                  </li>
-                </Link>
-              </div>
+                <svg
+                  focusable="false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 25 25"
+                >
+                  <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                </svg>
+              </span>
             </div>
-          ))}
+            {/* Search input */}
+            <p className={!isOpen ? 'block pl-1' : 'opacity-0 pl-1'}>
+              What are you looking for?
+            </p>
+          </div>
+          {/* Dropdown Menu */}
+          <div
+            id="dropdown"
+            className={`absolute z-10 pl-10 rounded-[22px] p-custom left-1/2 -translate-x-1/2 -translate-y-2.5 flex flex-col w-full ${
+              isOpen
+                ? 'block bg-transparent shadow-xl outline outline-1'
+                : 'hidden'
+            }`}
+          >
+            {/* Landing Option*/}
+            <div className="flex flex-row relative rounded-full gap-1.5 lg:w-1/2 md:w-2/3 sm:w-2/3 ">
+              <span className="h-6 w-6 -ml-[24px] mt-[8px] absolute">
+                <svg
+                  focusable="false"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 25 25"
+                >
+                  <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                </svg>
+              </span>
+              <li className=" block px-3 pt-2 pb-4 pl-5">
+                What are you looking for?
+              </li>
+            </div>
+            <div className="translate-x-4 -translate-y-2.5 border-b w-[90%] h-1 border-gray-300"></div>
+            {/* Options - Map through services state */}
+            {services.map((service, index) => (
+              <div key={index} className=" ">
+                <div className="flex flex-row relative ">
+                  <span className="h-6 w-6 -ml-[24px] mt-[8px] absolute">
+                    <svg
+                      focusable="false"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 25 25"
+                    >
+                      <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                    </svg>
+                  </span>
+                  <Link href={`/search-results?service=${service}`}>
+                    <li className="block px-5 py-2">
+                      I want{' '}
+                      <span className="font-roboto font-bold">{service}</span>
+                    </li>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      {/* I'm Feeling Lucky */}
-      <div className={`pt-4 ${!isOpen ? 'opacity-1' : 'opacity-0'}`}>
-        <Link href="/contact">
-          <button className="px-4 py-2 rounded-lg outline outline-1 hover:shadow-lg">
-            I&apos;m Feeling Lucky
-          </button>
-        </Link>
-      </div>
-    </>
-  )
+        {/* I'm Feeling Lucky */}
+        <div className={`pt-4 ${!isOpen ? 'opacity-1' : 'opacity-0'}`}>
+          <Link href="/contact">
+            <button className="px-4 py-2 rounded-lg outline outline-1 hover:shadow-lg">
+              I&apos;m Feeling Lucky
+            </button>
+          </Link>
+        </div>
+      </>
+    )
 }
