173fe12dc263fb99768c729f6751f88c5ed8fc29
Daphne Simons <daphnejasminesimons@gmail.com>
2024-12-19 14:53:54 +1300

    add webpack config so font files can be served

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Add a rule for handling font files
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/, // Match font file extensions
+      type: 'asset/resource', // Use Webpack's built-in asset/resource type
+      generator: {
+        filename: 'static/fonts/[name][ext]', // Output font files to /_next/static/fonts/
+      },
+    })
+
+    return config // Return the modified configuration
+  },
 }
 
 export default nextConfig
