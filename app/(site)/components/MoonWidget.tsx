91685030425e1c570e04415ee841f92fd7ebfa77
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2025-01-08 07:53:32 +1300

    refactor: simplified phase calculation

--- app/(site)/components/MoonWidget.tsx
+++ app/(site)/components/MoonWidget.tsx
@@ -6,6 +6,41 @@ import MoonLoader from './MoonLoader'
 import Link from 'next/link'
 import Image from 'next/image'
 
+const phases = [
+  {
+    name: 'New Moon',
+    img: 'new',
+  },
+  {
+    name: 'Waxing',
+    img: 'waxing',
+  },
+  {
+    name: 'First Quarter',
+    img: 'first-q',
+  },
+  {
+    name: 'Waxing Gibbous',
+    img: 'waxing-g',
+  },
+  {
+    name: 'Full Moon',
+    img: 'full',
+  },
+  {
+    name: 'Waning Gibbous',
+    img: 'waning-g',
+  },
+  {
+    name: 'Last Quarter',
+    img: 'last-q',
+  },
+  {
+    name: 'Wanning',
+    img: 'waning',
+  },
+]
+
 export default function MoonWidget({ size }: { size: string }) {
   const {
     data: moonData,
@@ -26,6 +61,7 @@ export default function MoonWidget({ size }: { size: string }) {
     //  Gets the dynamic svg data from the moonData api query:
     const moonPhaseSVG = moonData.phase[new Date().getDate()].svg
 
+    // FIX: it seems that the for loop below does nothing
     // List out all moonPhases from api:
     for (let key in moonData.phase) {
       if (moonData.phase.hasOwnProperty(key)) {
@@ -40,57 +76,46 @@ export default function MoonWidget({ size }: { size: string }) {
       }
     }
 
-    const imgArr = [
-      '/moon-imgs/new.png',
-      '/moon-imgs/waxing.png',
-      '/moon-imgs/first-q.png',
-      '/moon-imgs/waxing-g.png',
-      '/moon-imgs/full.png',
-      '/moon-imgs/waning-g.png',
-      '/moon-imgs/last-q.png',
-      '/moon-imgs/waning.png',
-    ]
-
     const getMoonImgByLighting = (lighting: number) => {
       if (lighting >= 0 && lighting <= 5) {
         return {
-          img: imgArr[0],
-          phase: 'New Moon',
+          img: phases[0].img,
+          phase: phases[0].name,
         }
       } else if (lighting >= 6 && lighting <= 42) {
         return {
-          img: imgArr[1],
-          phase: 'Waxing',
+          img: phases[1].img,
+          phase: phases[1].name,
         }
       } else if (lighting >= 43 && lighting <= 60) {
         return {
-          img: imgArr[2],
-          phase: 'First Quarter',
+          img: phases[2].img,
+          phase: phases[2].name,
         }
       } else if (lighting >= 61 && lighting <= 95) {
         return {
-          img: imgArr[3],
-          phase: 'Waxing Gibbous',
+          img: phases[3].img,
+          phase: phases[3].name,
         }
       } else if (lighting >= 96 && lighting <= 105) {
         return {
-          img: imgArr[4],
-          phase: 'Full Moon',
+          img: phases[4].img,
+          phase: phases[4].name,
         }
       } else if (lighting <= 97 && lighting >= 66) {
         return {
-          img: imgArr[5],
-          phase: 'Waning Gibbous',
+          img: phases[5].img,
+          phase: phases[5].name,
         }
       } else if (lighting <= 65 && lighting >= 44) {
         return {
-          img: imgArr[6],
-          phase: 'Last Quarter',
+          img: phases[6].img,
+          phase: phases[6].name,
         }
       } else if (lighting <= 43 && lighting >= 5) {
         return {
-          img: imgArr[7],
-          phase: 'Waning',
+          img: phases[7].img,
+          phase: phases[7].name,
         }
       }
     }
@@ -99,19 +124,20 @@ export default function MoonWidget({ size }: { size: string }) {
       moonData.phase[new Date().getDate()].lighting.toFixed(0)
     )
 
-    const moonImgData = getMoonImgByLighting(moonLighting)
+    const phase = getMoonImgByLighting(moonLighting)
+    const phaseImagePath = `/moon-imgs/${phase?.img}.png`
 
-    if (moonImgData) {
+    if (phase) {
       return (
         <div className="">
           <Link
             href="/moon"
             className="gap-6 hover:underline flex items-center"
           >
-            <h2>{moonImgData.phase}</h2>
+            <h2>{phase.phase}</h2>
             <Image
-              src={moonImgData.img}
-              alt={moonImgData.phase}
+              src={phaseImagePath}
+              alt={phase.phase}
               width={30}
               height={30}
               className=""
