a28f14b118bf7b79bee435abe775191b4467cac2
Daphne Simons <daphnejasminesimons@gmail.com>
2025-01-09 14:51:40 +1300

    add logic to show sanity gallery images in google style grid

--- app/(site)/components/SearchResults/AllProjects.tsx
+++ app/(site)/components/SearchResults/AllProjects.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Gallery from './Gallery'
+import { Project } from '@/types/project'
 
 interface Props {
   option: {
@@ -8,35 +9,37 @@ interface Props {
     cost: string
   }
   chosenService: string
-  chosenGemLevel: string
+  chosenProjects: Project[]
   handleClickSelection: (index: number) => void
 }
 export default function AllProjects({
   option,
   chosenService,
-  chosenGemLevel,
+  chosenProjects,
   handleClickSelection,
 }: Props) {
-  // Changes the gem selection heading colour:
+  // Changes heading colour according to gem:
   const headingColor: { [key: string]: string } = {
     Sapphire: 'text-blue-600',
     Emerald: 'text-green-600',
     Ruby: 'text-red-600',
   }
 
+  console.log('chosenProjects', chosenProjects)
+
   return (
     <div className="h-full ring-1 rounded-xl ring-[#7d8084]">
       {/* GALLERY Grid */}
       <div className="h-2/5 ">
-        <Gallery handleClickSelection={handleClickSelection} />
+        {/* TODO: prop drill chosenProjects into Gallery */}
+        <Gallery
+          handleClickSelection={handleClickSelection}
+          chosenProjects={chosenProjects}
+        />
       </div>
       {/* Text Part */}
       <div className="h-1/5 px-2 pt-2">
-        <h1
-          className={`${headingColor[chosenGemLevel]} ${headingColor[option.gem]} text-3xl`}
-        >
-          {option.gem}
-        </h1>
+        <h1 className={`${headingColor[option.gem]} text-3xl`}>{option.gem}</h1>
         <p className="pt-2 text-[#BDC1C5] text-sm">{option.level}</p>
       </div>
       <div className="px-2 pt-2 h-2/5 border-t  border-[#7d8084]">
--- app/(site)/components/SearchResults/Gallery.tsx
+++ app/(site)/components/SearchResults/Gallery.tsx
@@ -1,46 +1,46 @@
+import { Project } from '@/types/project'
+import Image from 'next/image'
+
 interface Props {
   handleClickSelection: (index: number) => void
+  chosenProjects: Project[]
 }
-export default function Gallery({ handleClickSelection }: Props) {
+export default function Gallery({
+  handleClickSelection,
+  chosenProjects,
+}: Props) {
+  // Define grid styles dynamically for each item
+  const gridStyles = [
+    'col-span-4 row-span-4 rounded-tl-xl',
+    'col-span-3 row-span-2',
+    'col-span-1 row-span-2 rounded-tr-xl',
+    'col-span-2 row-span-2',
+    'col-span-2 row-span-2',
+  ]
   return (
     <div
       id="gallery-grid"
       className="w-full h-full grid grid-cols-8 grid-rows-4 gap-1"
     >
-      {/* TODO - Add Images to Gallery */}
-      {/* TODO: each img should have a unique id, AND this ID is attached to a handleClick event. 
-      This will trigger a state to display the image and work details */}
-      {/* Gallery Item1 */}
-      <div
-        // Toggles to show selected work
-        onClick={() => handleClickSelection(1)}
-        id="1"
-        className="bg-blue-200 col-span-4 row-span-4 rounded-tl-xl"
-      ></div>
-      {/* Gallery Item2 */}
-      <div
-        onClick={() => handleClickSelection(1)}
-        id="2"
-        className="bg-blue-300 col-span-3 row-span-2"
-      ></div>
-      {/* Gallery Item3 */}
-      <div
-        onClick={() => handleClickSelection(1)}
-        id="3"
-        className="bg-blue-400 col-span-1 row-span-2 rounded-tr-xl"
-      ></div>
-      {/* Gallery Item4 */}
-      <div
-        onClick={() => handleClickSelection(1)}
-        id="4"
-        className="bg-blue-500 col-span-2 row-span-2"
-      ></div>
-      {/* Gallery Item5 */}
-      <div
-        onClick={() => handleClickSelection(1)}
-        id="5"
-        className="bg-blue-600 col-span-2 row-span-2"
-      ></div>
+      {/* TODO: map through chosenImages and render just like the HARDCODED GOOGLE STYLE IMG GRID- but dynamic*/}
+
+      {/* Map through chosenProjects and dynamically apply grid styles */}
+      {chosenProjects.slice(0, gridStyles.length).map((project, index) => (
+        <div
+          key={project._id}
+          id={project._id}
+          onClick={() => handleClickSelection(index)}
+          className={`relative overflow-hidden ${gridStyles[index]} `}
+        >
+          <Image
+            src={project.image}
+            alt={project.name}
+            width={0}
+            height={0}
+            className="rounded-lg fill object-cover"
+          />
+        </div>
+      ))}
     </div>
   )
 }
