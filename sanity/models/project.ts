b46b427cbec4005f30275fc6c327fbe67544aa28
Daphne Simons <daphnejasminesimons@gmail.com>
2025-01-20 16:38:35 +1300

    update client side Project model

--- sanity/models/project.ts
+++ sanity/models/project.ts
@@ -2,11 +2,10 @@ import { PortableTextBlock } from 'next-sanity'
 
 export type Project = {
   _id: string
-  _createdAt: Date
   name: string
-  slug: string
   image: string
   url: string
   gem: string
-  content: PortableTextBlock[] // This is how Sanity stores 'rich' content.
+  category: string
+  description: PortableTextBlock[] // This is how Sanity stores 'rich' content.
 }
