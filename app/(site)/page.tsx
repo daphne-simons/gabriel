b17f3281c4b5b9d7ab8178a46cdd154360895203
Daphne Simons <daphnejasminesimons@gmail.com>
2025-01-22 11:40:44 +1300

    tidy home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,14 +5,13 @@ import { Category } from '@/sanity/models/sanity-client-models'
 import { sanityFetch } from '@/sanity/config/client-config'
 
 export default async function Home() {
-  // TODO: Query Catgories and prop drill
+  // Query Catgories and prop drill into HomePage client component
   const categories: Category[] = await sanityFetch({
     query: categoriesQuery,
     // You can add multiple tags that matches with your document _id: ['project', 'post', etc]
     tags: ['category'],
   })
 
-  console.log({ categories })
   return (
     <>
       <Suspense>
