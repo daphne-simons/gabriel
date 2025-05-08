24c53e950fdab2b06db3d6144aea049a2a27f998
Daphne Simons <daphnejasminesimons@gmail.com>
2025-05-09 11:25:48 +1200

    fix moon page - too many layers

--- app/(site)/moon/page.tsx
+++ app/(site)/moon/page.tsx
@@ -2,18 +2,6 @@ import MoonLoader from '../components/MoonLoader'
 
 export default function MoonPage() {
   return (
-    <>
-      <div className="main-bg">
-        <div className=" bg-wrapper">
-          <div className="dots-1 dots"></div>
-          <div className="dots-2 dots"></div>
-          <div className="dots-3 dots"></div>
-          <div className="h-screen flex justify-center ">
-            <MoonLoader size="medMoon" />
-          </div>
-        </div>
-      </div>
-
-    </>
+    <MoonLoader size="medMoon" />
   )
 }
