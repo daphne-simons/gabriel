07e28d6b80afa2a90dbdd57f42c66e906cf40715
Daphne Simons <daphnejasminesimons@gmail.com>
2025-06-12 17:23:57 +1200

    fix white blur border again

--- app/(site)/components/BackGround.tsx
+++ app/(site)/components/BackGround.tsx
@@ -22,7 +22,7 @@ export default function BackGround({
       {/* Rock slice BG with blur/focus animation */}
       <div className="relative overflow-hidden min-h-screen">
         <div
-          className={`fixed inset-0 -inset-2 bg-cover bg-center ${theme.bgImg} animate-focus`}
+          className={`fixed -inset-4 bg-cover bg-center ${theme.bgImg} animate-focus`}
           style={{ zIndex: -1 }}
         ></div>
         <div className={`relative ${theme.textColor}`} style={{ zIndex: 10 }}>
