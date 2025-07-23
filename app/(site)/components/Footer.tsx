95b651205c81a3a008d3798d6d7c83a3e0312ed4
Daphne Simons <daphnejasminesimons@gmail.com>
2025-07-24 11:10:22 +1200

    get rid of TODO comment

--- app/(site)/components/Footer.tsx
+++ app/(site)/components/Footer.tsx
@@ -7,7 +7,6 @@ export default function Footer() {
     <div className="text-xs pt-8 flex flex-col max-md:w-full max-md:px-12 md:px-16 md:w-5/6 lg:text-sm xl:text-sm">
       <div className="flex flex-row pb-8">
         <p className="">Follow us</p>
-        {/* TODO: find out why this logo is not appearing in the real emails.  */}
         <div className="pl-4 -mt-[2.5px]">
           <Link href="https://www.instagram.com/gabriel_exchange/?utm_source=ig_web_button_share_sheet" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} className="w-6 h-6" />{' '}
