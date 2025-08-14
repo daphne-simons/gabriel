5d8dacde4feb3bc2023d0cbb98418322e13cdb19
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-15 11:32:50 +1200

    updated email to custom domain

--- app/(site)/utils/resend-utils.ts
+++ app/(site)/utils/resend-utils.ts
@@ -10,10 +10,12 @@ export async function enquiryProvider(person: UserQuery) {
   try {
     await resend.emails.send({
       // TODO: This from address will need to change to a verified domain on resend, e.g. 'contact@gabriel.com' or whatever Ella wants to use for the domain.
-      from: 'Acme <onboarding@resend.dev>',
+      // from: 'Acme <onboarding@resend.dev>',
+      from: 'more@gabriel.exchange',
       // TODO: this is the receiver address (or addresses, can send 50 max in the array)
       // use this one for testing success of delivery: 'delivered@resend.dev'
-      to: 'daphnejasminesimons@gmail.com',
+      // to: 'daphnejasminesimons@gmail.com',
+      to: 'more@gabriel.exchange',
       bcc: ['delivered@resend.dev'],
       subject: `Gabriel enquiry from ${person.name}!`,
       react: EnquiryEmail(person),
@@ -30,18 +32,20 @@ export async function autoReplyProvider(person: UserQuery) {
     // Send the email using Resend API
     await resend.emails.send({
       // TODO: This from address will need to change to a verified domain on resend, e.g. 'contact@gabriel.com' or whatever Ella wants to use for the domain.
-      from: 'onboarding@resend.dev',
+      // from: 'onboarding@resend.dev',
+      from: 'more@gabriel.exchange',
       // TODO: this is the receiver address, replace with dynamic person.email OR multiple addresses, can send 50 max in the array)
       // use this one for testing success of delivery: 'delivered@resend.dev'
       // ellas test email: 'ellasutherland@me.com' 
-      to: 'daphnejasminesimons@gmail.com',
+      // to: 'daphnejasminesimons@gmail.com',
+      to: person.email,
       bcc: ['delivered@resend.dev'],
       subject: `Auto-Reply to ${person.name}!`,
       react: AutoReply(person),
 
       // TODO: put Gabriel's email address here. For the user to 'reply_to'
-      // reply_to: 'more@gabriel.exchange',
-      reply_to: 'daphne.jasmine.simons@gmail.com'
+      // reply_to: 'daphne.jasmine.simons@gmail.com'
+      reply_to: 'more@gabriel.exchange',
     })
   } catch (error) {
     console.error('Error sending email:', error)
