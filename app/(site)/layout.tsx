b031fad7b1a89e455770cc90acb96cc41ca57e55
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-25 11:32:32 +1200

    delete example comment

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -6,7 +6,6 @@ import { Analytics } from "@vercel/analytics/next" // From Vercel; gives us the 
 import { SpeedInsights } from '@vercel/speed-insights/next'; // From Vercel; gives us the ability to observe the websites Performance metrics, to use, go to Speed Insights tab of project.
 
 const moonPhase = getMoonPhaseForWidget() // have to call this fn above the async function
-// Test comment! 
 
 export async function generateMetadata(): Promise<Metadata> {
   // Create dynamic favicon path based on current moon phase
