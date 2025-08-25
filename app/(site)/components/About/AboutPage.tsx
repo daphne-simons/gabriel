dd51ebf1a4d2c03a63802c9fcb3028ef554b7b04
Daphne Simons <daphnejasminesimons@gmail.com>
2025-08-25 12:23:42 +1200

    fix About Page bug

--- app/(site)/components/About/AboutPage.tsx
+++ app/(site)/components/About/AboutPage.tsx
@@ -49,90 +49,78 @@ export default function AboutPage({ data }: { data: AboutPageModel }) {
         </ul>
         {/* BODY */}
         <main className="pt-10 pb-24 px-20 max-md:px-4 ">
-          {/* Gabriel Description */}
+          {/* DESCRIPTION */}
           <section className="flex justify-center text-center ">
             <article className="font-roboto text-xl max-md:text-[20px] max-md:w-[80%] md:w-[85%] lg:w-[75%] xl:w-[50%] xl:text-2xl">
               <PortableText value={data.description} components={components} />
             </article>
           </section>
-          {/* Blocks/ Buttons of info */}
+          {/* BLOCKS OF INFO*/}
           <div className="pt-20 max-md:pt-16 flex flex-col items-center">
-            <section className="w-5/6 grid grid-cols-2 grid-rows-3 max-md:grid-cols-1 max-md:grid-rows-4 max-md:gap-6 md:gap-4 place-content-evenly">
-              <article className="sm:w-full md:w-[90%] font-roboto text-2xl ">
+            <section className="w-5/6 grid grid-cols-2 grid-rows-2 gap-16 max-md:grid-cols-1 max-md:grid-rows-4 max-md:gap-0 place-content-evenly">
+
+              {/* ADDRESS */}
+              <article className="sm:w-full md:w-[90%] max-md:pt-4 max-md:pb-0 font-roboto text-2xl ">
                 <h2>Gabriel</h2>
-                <div className="py-2 text-base max-md:pb-8 text-googleMidGray ">
+                <div className="py-2 text-base max-md:pb-4 pb-10 text-googleMidGray ">
                   <PortableText value={data.address} />
                 </div>
-                {/* SMALL - BUTTON 1 */}
                 <Link href="https://maps.app.goo.gl/2rd1gz6KkYwJTWgN9" target="_blank" rel="noopener noreferrer">
-                  <button className="w-32 h-10 flex items-center justify-center text-base text-googleBlue border border-2-lightGray hover:text-googleDarkBlue hover:border-googleBlue rounded-md px-6 py-2 md:hidden lg:hidden xl:hidden 2xl:hidden">
+                  <button className="w-32 h-10 flex items-center justify-center text-base text-googleBlue border border-2-lightGray hover:text-googleDarkBlue hover:border-googleBlue rounded-md px-6 py-2 ">
                     View Map
                   </button>
                 </Link>
               </article>
-              <article className="sm:w-full md:w-[90%] max-md:pt-8 font-roboto text-2xl ">
+              {/* CONTACT */}
+              <article className="sm:w-full md:w-[90%] max-md:pt-10 font-roboto text-2xl ">
                 <h2>Contact</h2>
-                <div className="pt-2 pb-16 text-base max-md:pb-8  text-googleMidGray">
+                <div className="pt-2 pb-16 max-md:pb-4 text-base text-googleMidGray">
                   <PortableText value={data.contact} />
                 </div>
-                {/* SMALL - BUTTON 2 */}
-                <a
-                  className="w-32 h-10 flex items-center justify-center text-base  text-googleBlue hover:text-googleDarkBlue border border-2-lightGray hover:border-googleBlue rounded-md md:hidden lg:hidden xl:hidden 2xl:hidden"
+                <Link
+                  className="w-32 h-10 flex items-center justify-center text-base  text-googleBlue hover:text-googleDarkBlue border border-2-lightGray hover:border-googleBlue rounded-md "
                   href="mailto:more@gabriel.exchange?subject=General%20Enquiry%20for%20Gabriel"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   Email Us
-                </a>
+                </Link>
               </article>
-              {/* MED -> LARGE Button 1 */}
-              <Link href="https://maps.app.goo.gl/2rd1gz6KkYwJTWgN9" target="_blank" rel="noopener noreferrer">
-                <button className="w-32 h-10 flex items-center justify-center text-base text-googleBlue border border-2-lightGray hover:text-googleDarkBlue hover:border-googleBlue rounded-md px-6 py-2 max-md:hidden">
-                  View Map
-                </button>
-              </Link>
-              {/* MED -> LARGE Button 2 */}
-              <a
-                className="w-32 h-10 flex items-center justify-center text-base  text-googleBlue hover:text-googleDarkBlue border border-2-lightGray hover:border-googleBlue rounded-md max-md:hidden"
-                href="mailto:more@gabriel.exchange?subject=General%20Enquiry%20for%20Gabriel"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Email Us
-              </a>
-              <article className="sm:w-full md:w-[90%] max-md:flex max-md:flex-col  max-md:justify-center font-roboto text-2xl">
+
+              {/* TEAM & CONSULTANTS*/}
+              <article className="sm:w-full md:w-[90%] max-md:flex max-md:flex-col max-md:pt-6 max-md:justify-center font-roboto text-2xl">
                 <h2>Team</h2>
                 <div className="pt-2 text-base text-googleMidGray">
                   <PortableText value={data.team} />
                 </div>
               </article>
-              <article className="sm:w-full md:w-[90%] max-md:flex max-md:flex-col  max-md:justify-start font-roboto text-2xl">
+              <article className="sm:w-full md:w-[90%] max-md:flex max-md:flex-col max-md:pt-6 max-md:justify-start font-roboto text-2xl">
                 Consultants
                 <div className="pt-2 text-base text-googleMidGray">
                   <PortableText value={data.consultants} />
                 </div>
               </article>
             </section>
-            {/* Seperate styling to switch between mob and laptops */}
+            {/* CLIENTS/ COLLABORATORS */}
             <div className="flex flex-col items-center">
-              <section className="w-5/6 pt-20 max-md:pt-0">
+              <section className="w-5/6 pt-14 max-md:pt-6">
                 <article className="w-11/12 text-2xl font-normal font-roboto">
                   Past Clients and Collaborators
                 </article>
-                <article className="max-md:w-full max-md:text-sm md:text-base pt-5 text-lg font-roboto text-googleMidGray">
+                <article className="pt-5 font-roboto  text-lg max-md:w-full max-md:text-base text-googleMidGray">
                   <PortableText value={data.clients_collaborators} />
                 </article>
               </section>
             </div>
           </div>
-        </main>
+        </main >
         {/* Footer */}
-        <div className="bg-googlelightGrayFooter bottom-0 w-full mt-auto">
+        < div className="bg-googlelightGrayFooter bottom-0 w-full mt-auto" >
           <div className="flex flex-col items-center">
             <Footer />
           </div>
-        </div>
-      </div>
+        </div >
+      </div >
     </>
   )
 }
