66eeaf636ab695cf09bfcb0e53f4e3b5b8e1d242
Daphne Simons <daphnejasminesimons@gmail.com>
2025-09-08 14:10:42 +1200

    fix more type errors

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -5,7 +5,7 @@ import HomeLogo from '../app/(site)/components/Logos/HomeLogo'
 test('Page', () => {
   // TODO: Fix this test.
   // render(<Home />)
-  render(<HomeLogo fontSettings={{ wght: 200, opsz: 72 }} />)
+  render(<HomeLogo fontSettings={{ wght: 200, opsz: 72 }} logoColor="text-white" />)
   expect(
     screen.getByRole('heading', { level: 1, name: 'Gabriel' })
   ).toBeDefined()
--- app/(site)/moon/page.tsx
+++ app/(site)/moon/page.tsx
@@ -2,18 +2,19 @@ import { sanityFetch } from '@/sanity/config/client-config'
 import { activeContributorsQuery, submissionsQuery } from '@/sanity/sanity-utils'
 import { Suspense } from 'react'
 import MoonPage from '../components/Constellation/MoonPage'
+import { ContributorModel, SubmissionModel } from '@/sanity/models/sanity-client-models'
 
 export default async function Moon() {
   // Timer for transition page
   // await new Promise((resolve) => setTimeout(resolve, 5000))
   // Query Catgories and prop drill into MoonPage 'use client' component
-  const contributors = await sanityFetch({
+  const contributors: ContributorModel[] = await sanityFetch({
     query: activeContributorsQuery,
     // You can add multiple tags that matches with your document _id: ['project', 'post', etc]
     tags: ['contributor'],
   })
 
-  const submissions = await sanityFetch({
+  const submissions: SubmissionModel[] = await sanityFetch({
     query: submissionsQuery,
     // You can add multiple tags that matches with your document _id: ['project', 'post', etc]
     tags: ['submission'],
